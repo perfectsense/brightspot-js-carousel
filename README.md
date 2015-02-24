42384374dc3e45bf4f0525e0835e8c92a6199607
Rusty Tanton <rusty@perfectsensedigital.com>
2015-02-24 12:26:45 -0500

    removed express demo, will redo with dist assets

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
 # brightspot-js-carousel
 
 More docs to follow.
-
-## Demo
-
-*	Run `bower install && npm install`
-*	Run `node demo/server.js`
-*	Visit http://localhost:3000
--- demo/server.js
+++
@@ -1,7 +0,0 @@
-var path = require('path');
-var express = require('express');
-var server = express();
-server.use('/assets', express.static( path.resolve(__dirname, '../') ));
-server.use(express.static( path.resolve(__dirname, '../demo') ));
-server.listen(3000);
-console.log('Listening on http://localhost:3000...');
