e135a3f95de96842854934a7c52724d01b23ae2b
Rusty Tanton <rusty@perfectsensedigital.com>
2015-03-27 09:43:58 -0400

    fixed requirejs demo

--- demo/require-test.js
+++ demo/require-test.js
@@ -2,6 +2,7 @@ require.config({
 	paths: {
 		'bsp-carousel': '../dist/bsp-carousel',
 		'bsp-carousel-plugin': '../dist/bsp-carousel-plugin',
+		'bsp-carousel-thumbnav': '../dist/bsp-carousel-thumbnav',
 		'bsp-carousel-thumbnav-plugin': '../dist/bsp-carousel-thumbnav-plugin',
 		'bsp-utils': '../bower_components/bsp-utils/bsp-utils',
 		'jquery': '../bower_components/jquery/dist/jquery'
