f9c3fe2b8349c10c047c8cc38bbb4274b5cd703d
Tom Hoppe <trhoppe@gmail.com>
2015-10-20 10:51:46 -0600

    Well that's a helluva bug. Accidentally removed the carousel gallery

--- src/js/bsp-carousel-gallery-plugin.js
+++ src/js/bsp-carousel-gallery-plugin.js
@@ -1,11 +1,11 @@
 import $ from 'jquery';
 import bsp_utils from 'bsp-utils';
-import bsp_carousel from 'bsp-carousel';
+import bsp_carousel_gallery from './bsp-carousel-gallery';
 
 export default bsp_utils.plugin(false, 'bsp', 'carousel-gallery', {
     '_each': function(item) {
         var options = this.option(item);
-        var moduleInstance = Object.create(bsp_carousel);
+        var moduleInstance = Object.create(bsp_carousel_gallery);
         moduleInstance.init($(item), options);
     }
 });
